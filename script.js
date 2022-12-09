98e4af3fd374f832f13511fb9534b3ed63f6f0e1
zocc-n <znanic@yahoo.com>
2022-12-09 03:27:03 +0100

    Fix major bugs. Add game and playRound functions.

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ function getComputerChoice(){
     if(randNum >= 0.6666){
        computerChoice = 'rock';
     }
-    else if(randNum < 0.6666 || randNum >= 0.3333){
+    else if(randNum < 0.6666 && randNum >= 0.3333){
         computerChoice = 'paper';
     }
     else{
         computerChoice = 'scissors'
     }
     console.log(randNum);
+    console.log('Computer choice: ' + computerChoice);
     return computerChoice;
 }
 
@@ -18,12 +19,35 @@ function getComputerChoice(){
 function getPlayerChoice(){
     let playerChoice = prompt('Rock, paper or scissors?').toLowerCase();
     if(playerChoice == 'rock' || playerChoice == 'paper' || playerChoice == 'scissors'){
-        console.log(playerChoice);
+        console.log('Plyer choice: ' + playerChoice);
         return playerChoice;    
     }else{
-        console.log('Invalid input.')
+        console.log('Invalid input.');
         getPlayerChoice();
     }
 }
 
-getPlayerChoice();
+
+function playRound(computerSelection, playerSelection){
+    if(computerSelection == playerSelection){
+        console.log('Draw');
+    }
+    else if((computerSelection == 'rock' && playerSelection == 'scissors') || 
+    (computerSelection == 'paper' && playerSelection == 'rock') ||
+    (computerSelection == 'scissors' && playerSelection == 'paper')){
+        console.log('You lose');
+    }
+    else{
+        console.log('You win');
+    }
+}
+
+
+function game(){
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+    playRound(computerSelection, playerSelection);
+}
+
+
+game();
